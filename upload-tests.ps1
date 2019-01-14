72d68ee4fe23e8be004cf363aca7b907a7e2feef
Victorma Perez Colado <victormp@ucm.es>
2019-01-14 22:52:12 +0100

    Init commit

---
+++ upload-tests.ps1
@@ -0,0 +1,5 @@
+
+$testsFile = Resolve-Path ".\TestResultV3_Passed.xml";
+
+$wc = New-Object 'System.Net.WebClient'
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", $testsFile)
